fd154c27efff62ea6cde912f5c7b3d8a1ac1535e
jonatassx96 <jonatas.sx96@gmail.com>
2023-06-06 22:24:10 -0300

    feat: create model $ controller to products

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express'
+import { resolve } from 'path'
 import routes from './routes'
+
 import './database'
 
 class App {
@@ -11,6 +13,10 @@ class App {
 
   middlewares() {
     this.app.use(express.json())
+    this.app.use(
+      '/product-file',
+      express.static(resolve(__dirname, '..', 'uploads'))
+    )
   }
 
   routes() {
